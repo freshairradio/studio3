820ab8c065db47a0982011b2eed334390d70a47f
rphi <8237136+rphi@users.noreply.github.com>
2020-10-08 18:35:30 +0100

    Probably good enough to go? No off-air playlist

--- bot2.py
+++ bot2.py
@@ -3,6 +3,8 @@ import asyncio
 import discord
 import youtube_dl
 import yaml
+import glob
+import random
 
 from discord.ext import commands
 
@@ -42,6 +44,10 @@ voice_controller_name = config['player_control']
 
 playqueue = []
 
+played_since_jingle = 0
+jingle_every_n_tracks = config['jingle_every_n_tracks']
+jingles = []
+
 class YTDLSource():
     def __init__(self, source, *, data):
         self.data = data
@@ -61,13 +67,18 @@ class YTDLSource():
         filename = data['url'] if stream else ytdl.prepare_filename(data)
         return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
 
+def setup_jingles():
+  global jingles
+  print(f'Looking for jingles in {config["jingle_search_path"]}...')
+  jingles = glob.glob(config['jingle_search_path'])
+  print(f'Found {len(jingles)} jingles.')
+  pass
 
 bot = commands.Bot(command_prefix=commands.when_mentioned_or("!"),
-                description='Relatively simple music bot example')
+                description='freshair radio Discord scheduler')
 
 async def setup_voicechans():
   global voice_channel
-  global voice_client
   global voice_controller
   print('Setting up playout voice channel...')
 
@@ -79,7 +90,9 @@ async def setup_voicechans():
 
   print(voice_channel)
 
-  print(f'Joining {voice_channel_name}')
+async def connect_voice_client():
+  global voice_client
+  print('Connecting to voice...')
   voice_client = await voice_channel.connect()
   print(voice_client)
 
@@ -100,11 +113,27 @@ def track_finished(e=None):
   else:
     coro = voice_controller.send("No more tracks in the queue... :(")
     asyncio.run_coroutine_threadsafe(coro, voice_client.loop)
+    print('Disconnecting from voice...')
+    asyncio.run_coroutine_threadsafe(voice_controller.disconnect(), voice_client.loop)
 
 async def play_next():
+  global played_since_jingle
+
+  if jingle_every_n_tracks <= played_since_jingle:
+    print('playing a jingle!')
+    played_since_jingle = 0
+    if len(jingles) == 0:
+      print('No jingles loaded... :(')
+    else:
+      jingle = random.choice(jingles)
+      player = discord.FFmpegPCMAudio(random.choice(jingles), **ffmpeg_options)
+      voice_client.play(player, after=track_finished)
+      return
+
   next_track = playqueue.pop(0)
   print(f'attempting to play {next_track["song"].title}')
   voice_client.play(next_track['song'].source, after=track_finished)
+  played_since_jingle += 1
   await voice_controller.send(f'Now playing: {next_track["song"].title}')
 
 @commands.check(check_if_in_controller)
@@ -125,22 +154,45 @@ async def play(ctx, *, url):
       await play_next()
 
 @commands.check(check_if_in_controller)
+@commands.has_role('admin')
 @bot.command(name='stop')
 async def stop(ctx):
-    """Stops and disconnects the bot from voice"""
+    """Stops current playout and disconnects from voice"""
 
     await ctx.voice_client.stop()
+    await voice_client.disconnect()
+
+@commands.check(check_if_in_controller)
+@commands.has_role('committee')
+@bot.command(name='skip')
+async def skip(ctx):
+    """Skips current track"""
+
+    await ctx.voice_client.stop()
+    await play_next()
+    await ctx.send(f'{ctx.author} has skipped the current track.')
+
+@commands.check(check_if_in_controller)
+@commands.has_role('committee')
+@bot.command(name='clear')
+async def clear(ctx):
+    """Stops playout and clears playlist"""
+
+    await ctx.voice_client.stop()
+    playqueue = []
+    await ctx.send(f'{ctx.author} has cleared the play queue.')
 
 @bot.command(name='ping')
 @commands.has_role('admin')
 async def ping(ctx):
-    await ctx.send('PONG! Yup, I\'m alive :D')
+  """Check the bot is responding..."""
+  await ctx.send('PONG! Yup, I\'m alive :D')
 
 @play.before_invoke
 @stop.before_invoke
 async def ensure_voice(ctx):
   if ctx.voice_client is None:
-    await setup_voicechans()
+    await connect_voice_client()
 
 @bot.event
 async def on_ready():
@@ -152,5 +204,7 @@ async def on_ready():
 async def on_command_error(ctx, error):
   if isinstance(error, commands.errors.CheckFailure):
     await ctx.send('You do not have the correct role for this command.')
+
+setup_jingles()
 
 bot.run(config['discord_token'])
